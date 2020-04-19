0b9bab6c53889bf295b2cf68097a94a08f40f8be
Giguk <47025909+Giguk@users.noreply.github.com>
2020-04-19 15:30:26 +0200

    SkillTree UI
    Version1.0 only simply functions

---
+++ Assets/Self/Scripts/SkillTree/SkillTree_Behavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTree_Behavior : MonoBehaviour
+{
+    private bool SkillTreeBool = false;
+    public int skillPoints = 0;
+    public GameObject skillTreeOBJ;
+    public Skill_Behavior doubleJump, playerSpeed, grenadeSpeed;
+
+    void Update()
+    {
+     
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SkillTree();
+        }
+
+        if (skillTreeOBJ)
+        {
+            skillTreeOBJ.SetActive(true);
+        }
+        else { skillTreeOBJ.SetActive(false); }
+
+    }
+
+    public void SkillTree()
+    {
+        SkillTreeBool = !SkillTreeBool;
+    }
+}
---
+++ Assets/Self/Scripts/SkillTree/Skill_Behavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Skills
+{
+    skill_DoubleJump,
+    skill_PlayerSpeed,
+    skill_GrenadeSpeed,
+}
+
+public class Skill_Behavior : MonoBehaviour
+{
+    public Skills thisSkill = Skills.skill_DoubleJump;
+    public SkillTree_Behavior tree;
+    public bool isPurchased = false;
+
+    private Skill_Behavior requiredSkill;
+    private Text textSkill;
+
+    void Start()
+    {
+        textSkill = GetComponentInChildren<Text>();
+
+        if (thisSkill.Equals(Skills.skill_DoubleJump)) { requiredSkill = tree.playerSpeed; requiredSkill = tree.grenadeSpeed; }
+
+    }
+
+    public void RankUp()
+    {
+        if (tree.skillPoints > 0 && !isPurchased)
+        {
+            if (thisSkill.Equals(Skills.skill_GrenadeSpeed))
+            {
+                tree.skillPoints -= 1;
+                isPurchased = true;
+                textSkill.text = "Purchased";
+                GrenadeSpeed();
+            }
+            if (thisSkill.Equals(Skills.skill_PlayerSpeed))
+            {
+                tree.skillPoints -= 1;
+                isPurchased = true;
+                textSkill.text = "Purchased";
+                PLayerSpeed();
+            }
+            else if (requiredSkill.isPurchased)
+            {
+                tree.skillPoints -= 1;
+                isPurchased = true;
+                textSkill.text = "Purchased";
+                if (thisSkill.Equals(Skills.skill_DoubleJump)) { DoubleJump(); }
+            }
+        }
+    }
+
+    private void GrenadeSpeed()
+    {
+
+    }
+
+    private void PLayerSpeed()
+    {
+
+    }
+
+    private void DoubleJump()
+    {
+
+    }
+}
